4ed04c0644ef5e81f9a99ed0915fa61cd1e545f4
Matthias <mefju7@gmail.com>
2016-04-06 18:37:32 +0200

    started removing coll bag

--- CollectThumbs/CollectThumbnails.cs
+++ CollectThumbs/CollectThumbnails.cs
@@ -10,8 +10,11 @@ namespace ThumbCollector
     internal class CollectThumbnails
     {
         private const int ChunkSize = 3 * 16 * 16;
-        ConcurrentBag<Task> allTasks=new ConcurrentBag<Task>();
+        private const int DisplayDelay = 2;
+        Semaphore sem = new Semaphore(1, 1);
+        // ConcurrentBag<Task> allTasks = new ConcurrentBag<Task>();
         object myLock = new object(); // just for synchronizing
+        long saved = 0;
         private StreamWriter txtFile;
         private FileStream binFile;
 
@@ -24,67 +27,78 @@ namespace ThumbCollector
             using (txtFile = File.CreateText("files.txt"))
             using (binFile = File.OpenWrite("files.bin"))
             {
+                var wait4it = new Wait4It(sem);
                 foreach (var a in args)
                 {
                     var t = Task.Run(() => scan(a));
-                    allTasks.Add(t);
+                    // allTasks.Add(t);
                 }
-                Task tw4;
-                while (allTasks.TryTake(out tw4))
+                Task t2w4;
+                var last = DateTime.Now.AddSeconds(DisplayDelay);
+                long finished = 0;
+                while (allTasks.TryTake(out t2w4))
                 {
-                    // Console.Out.WriteLine("waiting for task");
-                    tw4.Wait();
+                    ++finished;
+                    if (t2w4.IsCompleted)
+                        continue;
+                    t2w4.Wait();
+                    var n=DateTime.Now;
+                    if(n > last)
+                    {
+                        var t = allTasks.Count;
+                        Console.WriteLine("{0} / {1} => {2}", finished, finished+t,saved);
+                        last = n.AddSeconds(DisplayDelay);
+                    }
                 }
-               
+
             }
         }
 
         private void scan(string dir)
         {
-            Console.Out.WriteLine("Scanning for {0}", dir);
-           
-            if (Directory.Exists(dir))
+            // Console.Out.WriteLine("Scanning for {0}", dir);
+            try
             {
-               var dirs=Directory.GetDirectories(dir);
-                foreach(var d in dirs)
+                if (Directory.Exists(dir))
                 {
-                    var t = Task.Run(() => scan(d));
-                    allTasks.Add(t);
-                }
-                var files = Directory.EnumerateFiles(dir);
-                foreach(var f in files)
-                {
-                    var t = Task.Run(() => getThumb(f));
-                    allTasks.Add(t);
-                    if (allTasks.Count > 1000) {
-                        Console.Out.WriteLine("sleeping");
-                        Thread.Sleep(5000);
+                    var dirs = Directory.GetDirectories(dir);
+                    foreach (var d in dirs)
+                    {
+                        var t = Task.Run(() => scan(d));
+                        allTasks.Add(t);
+                    }
+                    var files = Directory.EnumerateFiles(dir);
+                    foreach (var f in files)
+                    {
+                        var t = Task.Run(() => getThumb(f));
+                        allTasks.Add(t);
                     }
                 }
             }
+            catch (Exception) { }
         }
 
         private void getThumb(string f)
         {
-            Console.Out.WriteLine("creating thumb for {0}", f);
             try
             {
                 using (var img = Image.FromFile(f))
                 {
+                    // Console.Out.WriteLine("creating thumb for {0}", f);
                     int h = img.Height;
                     int w = img.Width;
                     if (h > 16) h = 16;
                     if (w > 16) w = 16;
                     using (var bmp = new Bitmap(img, w, h))
                     {
-                        var b=new byte[ChunkSize];
-                        for(int i = 0; i < bmp.Height; ++i)
+                        var b = new byte[ChunkSize];
+                        for (int i = 0; i < bmp.Height; ++i)
                         {
                             int row = 3 * 16 * i;
-                            for(int j = 0; j < bmp.Width; ++j)
+                            for (int j = 0; j < bmp.Width; ++j)
                             {
                                 int idx = row + j * 3;
-                                var c=bmp.GetPixel(j, i);
+                                var c = bmp.GetPixel(j, i);
                                 b[idx] = c.R;
                                 b[idx + 1] = c.G;
                                 b[idx + 2] = c.B;
@@ -94,7 +108,7 @@ namespace ThumbCollector
                     }
                 }
             }
-            catch(OutOfMemoryException )
+            catch (OutOfMemoryException)
             {
                 // ignore
             }
@@ -103,11 +117,14 @@ namespace ThumbCollector
 
         private void store(string f, byte[] b)
         {
-            lock(myLock)
+            lock (myLock)
             {
                 txtFile.WriteLine(f);
                 binFile.Write(b, 0, ChunkSize);
+                ++saved;
             }
         }
+
+       
     }
 }
---
+++ CollectThumbs/Wait4It.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+
+namespace ThumbCollector
+{
+    internal class Wait4It
+    {
+        private Semaphore sem;
+
+        public Wait4It(Semaphore sem)
+        {
+            this.sem = sem;
+            sem.WaitOne();
+        }
+        ~Wait4It()
+        {
+            sem.Release();
+        }
+       
+    }
+}
