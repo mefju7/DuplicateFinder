a86a8afb557c4d329dae6f22fd8e74b6f2e14ca6
Matthias <mefju7@gmail.com>
2016-04-08 19:04:20 +0200

    ok and greit

--- DuplicateViewer/Form1.Designer.cs
+++ DuplicateViewer/Form1.Designer.cs
@@ -36,10 +36,13 @@
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.statusLine = new System.Windows.Forms.ToolStripStatusLabel();
             this.progress = new System.Windows.Forms.ToolStripProgressBar();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPanel = new System.Windows.Forms.TableLayoutPanel();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -49,7 +52,7 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(698, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(912, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -65,14 +68,14 @@
             // openSetFileToolStripMenuItem
             // 
             this.openSetFileToolStripMenuItem.Name = "openSetFileToolStripMenuItem";
-            this.openSetFileToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.openSetFileToolStripMenuItem.Size = new System.Drawing.Size(168, 26);
             this.openSetFileToolStripMenuItem.Text = "&Open set file";
             this.openSetFileToolStripMenuItem.Click += new System.EventHandler(this.openSetFileToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(168, 26);
             this.exitToolStripMenuItem.Text = "E&xit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -80,14 +83,15 @@
             // 
             this.trackBar1.Dock = System.Windows.Forms.DockStyle.Right;
             this.trackBar1.LargeChange = 1;
-            this.trackBar1.Location = new System.Drawing.Point(642, 28);
+            this.trackBar1.Location = new System.Drawing.Point(856, 28);
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
             this.trackBar1.RightToLeftLayout = true;
-            this.trackBar1.Size = new System.Drawing.Size(56, 560);
+            this.trackBar1.Size = new System.Drawing.Size(56, 661);
             this.trackBar1.TabIndex = 1;
             this.trackBar1.TickFrequency = 0;
             this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
             // 
             // statusStrip1
             // 
@@ -95,9 +99,9 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusLine,
             this.progress});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 563);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 664);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(642, 25);
+            this.statusStrip1.Size = new System.Drawing.Size(856, 25);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -112,22 +116,44 @@
             this.progress.Name = "progress";
             this.progress.Size = new System.Drawing.Size(100, 19);
             // 
-            // panel1
+            // pictureBox1
             // 
-            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 28);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(642, 535);
-            this.panel1.TabIndex = 3;
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(846, 626);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPanel
+            // 
+            this.tabPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tabPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.tabPanel.ColumnCount = 1;
+            this.tabPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tabPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tabPanel.Controls.Add(this.pictureBox1, 0, 0);
+            this.tabPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabPanel.Location = new System.Drawing.Point(0, 28);
+            this.tabPanel.Name = "tabPanel";
+            this.tabPanel.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPanel.RowCount = 1;
+            this.tabPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tabPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tabPanel.Size = new System.Drawing.Size(856, 636);
+            this.tabPanel.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 588);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(912, 689);
+            this.Controls.Add(this.tabPanel);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.menuStrip1);
@@ -139,6 +165,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,7 +182,8 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel statusLine;
         private System.Windows.Forms.ToolStripProgressBar progress;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tabPanel;
     }
 }
 
--- DuplicateViewer/Form1.cs
+++ DuplicateViewer/Form1.cs
@@ -17,6 +17,7 @@ namespace DuplicateViewer
         public Form1()
         {
             InitializeComponent();
+            progress.Visible = false;
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -31,10 +32,13 @@ namespace DuplicateViewer
             if(r== DialogResult.OK)
             {
                 var lines = File.ReadAllLines(ofd.FileName);
+                progress.Value = 0;
+                progress.Visible = true;
                 progress.Maximum = lines.Length;
                 fileSets.Clear();
                 var lastSet =new  List<String>();
                 int i = 0;
+                int setMax = 0;
                 foreach (var line in lines)
                 {
                     progress.Value = ++i;
@@ -49,11 +53,46 @@ namespace DuplicateViewer
                         lastSet = nl;
                         fileSets.Add(nl);
                     }
+                    if (lastSet.Count > setMax)
+                        setMax = lastSet.Count;
                 }
                 trackBar1.Minimum = 0;
-                trackBar1.Maximum = fileSets.Count;
+                trackBar1.Maximum = fileSets.Count-1;
                 statusLine.Text = string.Format("{0} sets of total {1} files", fileSets.Count, lines.Length);
+                for(int c = 1; c < setMax; ++c)
+                {
+                    if(c*c > setMax)
+                    {
+                        tabPanel.ColumnCount = c;
+                        tabPanel.RowCount = c;
+                        break;
+                    }
+                }
+                progress.Visible = false;
+                trackBar1.Value = trackBar1.Maximum;
             }
         }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            int num = trackBar1.Maximum - trackBar1.Value;
+            tabPanel.Controls.Clear();
+     
+            var picList = fileSets[num];
+            int cols = 1;
+            for (; cols * cols < picList.Count; ++cols) ;
+            for (int i = 0; i < picList.Count; ++i)
+            {
+                var pb = new PictureBox();
+                pb.SizeMode = PictureBoxSizeMode.Zoom;
+            
+                pb.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
+                tabPanel.Controls.Add(pb,i% cols,i/cols);
+                pb.LoadAsync(picList[i]);
+            }
+            statusLine.Text = string.Format("loaded set {0}", num + 1);
+        }
+
+       
     }
 }
